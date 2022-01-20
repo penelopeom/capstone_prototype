051291be944765da4ce810480180176462e781ce
sarcho22 <sarcho22@bergen.org>
2022-01-20 13:15:27 -0500

    Update demo_mysql_connection.py

--- scripts/demo_mysql_connection.py
+++ scripts/demo_mysql_connection.py
@@ -3,7 +3,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com",
   user="njccic_usr",
-  password="iJdf56*kf"
+  password="iJdf56*kf",
+  database="njccic_capstone"
 )
 
 print(mydb)
