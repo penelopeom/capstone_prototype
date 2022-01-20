256229d123d4b021a484f6e4bfb95edff2f1d523
sarcho22 <sarcho22@bergen.org>
2022-01-20 13:36:48 -0500

    code efficiency !!!
    got rid of elif statements !!!

--- scripts/big_script.py
+++ scripts/big_script.py
@@ -10,25 +10,14 @@ mydb = mysql.connector.connect( # add proper credentials
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT request_id, request_type FROM requests WHERE result is null")
+mycursor.execute("SELECT request_id, request_type, address FROM requests WHERE result is null")
 
 myresult = mycursor.fetchall()
 
 for x in myresult: # pretend the scripts genuinely exist
-    print(x)
     type = x[1].strip()
-    print(type)
+    address = x[2].strip()
 
-    if type == "contact":
-        os.system("python3 scripts/contact.py")
-    elif type == "geolocation":
-        os.system("python3 scripts/geolocation.py")
-    elif type  == "nslookup":
-        os.system("python3 scripts/nslookup.py")
-    elif type == "overview":
-        os.system("python3 scripts/overview.py")
-    elif type == "socials":
-        os.system("python3 scripts/socials.py")
-    else:
-        print("LLLLLL stoopid")
+    os.system("python3 scripts/" + type + ".py " + address)
+
 
