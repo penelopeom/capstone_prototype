045a2486341b3fcb452dc452eed5e033e7973093
penelopeom <penelopeom@aol.com>
2022-02-26 00:30:02 -0500

    file upload
    got file upload working (no guarantee on how the results are outputted though)

---
+++ file_input.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>File Input Page</title>
+</head>
+  
+<body>
+    <center>
+        <?php
+  
+        session_start();
+        $user = 'njccic_usr';
+        $password = 'iJdf56*kf'; 
+        $database = 'njccic_capstone'; 
+        $servername='soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com';
+        
+        $conn = mysqli_connect($servername, $user, $password, $database);
+          
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+          
+        // Taking txt file of IP addresses from the form data(input) and request type
+        $input =  $_FILES['myfile']['tmp_name'];
+        $ip = file_get_contents($input);
+        $request_type = $_REQUEST['request_type'];
+          
+        // Performing insert query execution
+        $id = uniqid();
+        $_SESSION['id'] = $id;
+        $sql = "INSERT INTO requests (request_id, request_type, address, result) VALUES ('$id','$request_type', '$ip', '')";
+          
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+  
+            echo nl2br("\n$request_type\n$ip");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+
+        // Close connection
+        mysqli_close($conn);
+        header("Location: /queue_page.php");
+        ?>
+    </center>
+</body>
+  
+</html>
--- scripts/whois_script.py
+++ scripts/whois_script.py
@@ -4,16 +4,23 @@ main(['install', 'ipwhois'])
 from ipwhois import IPWhois
 
 def whois_func(address):
-    IPaddress = address.strip()
-    search = IPWhois(IPaddress)
-    res = search.lookup_whois()
-    if (res != ""):
-        address = res["nets"][0]['address']
-        city = res["nets"][0]['city']
-        country = res["nets"][0]['country']
-        username = res["nets"][0]['name']
-        
-        fullAddr = address + ", " + city + ", " + country
-        return username + ", " + fullAddr
+    # if single IP, there will only be one element of the array after split
+    # if multiple IPs, it will convert them into an array
+    addresses = address.split("\r\n")
+    result = ""
 
-    return "NO RESULTS FOUND"
+    for IPaddress in addresses:
+        search = IPWhois(IPaddress)
+        res = search.lookup_whois()
+        if (res != ""):
+            address = res["nets"][0]['address']
+            city = res["nets"][0]['city']
+            country = res["nets"][0]['country']
+            username = res["nets"][0]['name']
+            
+            fullAddr = address + ", " + city + ", " + country
+            result += username + ", " + fullAddr + ". ";
+        else:
+            result += "NO RESULTS FOUND"
+
+    return result
