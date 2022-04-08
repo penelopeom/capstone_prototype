cc2eb60aa54165ef4a21fa316224b78d5bee69cc
Devin Crowley <devcro22@bergen.org>
2022-04-08 14:33:33 -0400

    continuing contact script
    adding functionality to get to ip's site

--- scripts/contact_script.py
+++ scripts/contact_script.py
@@ -11,36 +11,76 @@ import geolocation_script
 us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 def contact_func(address):
-    # Creating an instance
+
+    # creating driver
     owner = whois_script.whois_func(address)
     driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
 
-    # Logging into LinkedIn
-    # driver.get("https://linkedin.com/uas/login")
-    # time.sleep(5)
-
-    # username = driver.find_element_by_id("username")
-    # username.send_keys("boibevin@gmail.com") # Enter Your Email Address
-
-    # pword = driver.find_element_by_id("password")
-    # pword.send_keys("bevinboibevin")	 # Enter Your Password
-
-    # driver.find_element_by_xpath("//button[@type='submit']").click()
-
+    
+    # parsing together search query
     location = geolocation_script.geolocation_func(address)
     location.split(' ')
-
     state = set(location) & set(us_states)
-
     profile_url = getURL(owner, state)
 
-    driver.get(profile_url)	 # this will open the link
+    # this will open the link
+    driver.get(profile_url)
 
-def getURL(companyName, State):
-    try:
-        term = ' '.join([companyName, State, "LinkedIn"])
-        for url in search(term, num_results=1):
-            return url
-    except:
-        return ''
+    # scrolling to the bottom
+    start = time.time()
+    initial_scroll = 0
+    final_scroll = 1000
+    pause_time = 3
+    scroll_time = 20
+
+    while True:
+        driver.execute_script(f"window.scrollTo({initial_scroll}, {final_scroll}")
+        initial_scroll = final_scroll
+        finalScroll += 1000
+
+        # stop so data can load
+        time.sleep(pause_time)
+        end = time.time()
+
+        # scroll for scroll time
+        if round(end - start) > scroll_time:
+            break
+
+    # getting page source to parse
+    src = driver.page_source
+    soup = BeautifulSoup(src, 'lxml')
+
+    # finding overview section
+    overview = soup.find('div', {'id': 'ember53','class': 'ember-view'})
+
+    web_loc = overview.findAll('a')
+    website = web_loc.get_text()
+
+    driver.get(website)
+
+    while True:
+        driver.execute_script(f"window.scrollTo({initial_scroll}, {final_scroll}")
+        initial_scroll = final_scroll
+        finalScroll += 1000
+
+        # stop so data can load
+        time.sleep(pause_time)
+        end = time.time()
+
+        # scroll for scroll time
+        if round(end - start) > scroll_time:
+            break
+
+    src = driver.page_source
+    contact = soup.find('a')
+
+
+
+    def getURL(companyName, State):
+        try:
+            term = ' '.join([companyName, State, "LinkedIn"])
+            for url in search(term, num_results=1):
+                return url
+        except:
+            return ''
         
