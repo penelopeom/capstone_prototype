a97de608c1f7410c9a24d2e9ec6bfe8d95d7a5b9
sarcho22 <sarcho22@bergen.org>
2022-04-08 15:19:48 -0400

    chrome stuff driver thing trying to do contact script??

--- scripts/contact_script.py
+++ scripts/contact_script.py
@@ -1,27 +1,55 @@
+from pip._internal import main
+main(['install','selenium'])
+from pip._internal import main
+main(['install','bs4'])
+from pip._internal import main
+main(['install','webdriver_manager'])
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
+from pip._internal import main
+main(['install','requests'])
 import requests
+from pip._internal import main
+main(['install','googlesearch-python'])
 from googlesearch import search
 import time
 import whois_script
 import geolocation_script
 
+
 # all us states 
 us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+def getURL(companyName, State):
+    try:
+        term = ' '.join([companyName, State, "LinkedIn"])
+        for url in search(term, num_results=1):
+            return url
+    except:
+        return ''
+
 
 def contact_func(address):
 
     # creating driver
     owner = whois_script.whois_func(address)
-    driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
+    # driver = webdriver.Chrome("Chrome\Application\chrome.exe")
 
-    
+    s=Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.get('https://www.google.com')
+
     # parsing together search query
     location = geolocation_script.geolocation_func(address)
     location.split(' ')
     state = set(location) & set(us_states)
     profile_url = getURL(owner, state)
+    print(profile_url)
 
     # this will open the link
     driver.get(profile_url)
@@ -72,15 +100,8 @@ def contact_func(address):
             break
 
     src = driver.page_source
-    contact = soup.find('a')
+    contact = soup.find_all("html", string="contact")["href"]
+    print(contact)
 
-
-
-    def getURL(companyName, State):
-        try:
-            term = ' '.join([companyName, State, "LinkedIn"])
-            for url in search(term, num_results=1):
-                return url
-        except:
-            return ''
+contact_func("12.150.236.18")
         
