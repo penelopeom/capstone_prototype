d8251a676ddb36d61116a4bbb3ef2e2959a66e4e
penelopeom <penelopeom@aol.com>
2022-04-21 14:51:39 -0400

    got googlesearch to return proper contact page

--- scripts/contact_script.py
+++ scripts/contact_script.py
@@ -21,11 +21,11 @@ import whois_script
 import geolocation_script
 
 # all us states 
-us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'NewHampshire', 'NewJersey', 'NewMexico', 'NewYork', 'NorthCarolina', 'NorthDakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'RhodeIsland', 'SouthCarolina', 'SouthDakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'WestVirginia', 'Wisconsin', 'Wyoming']
 
 def getURL(companyName, State):
     try:
-        term = ' '.join([companyName, State, "LinkedIn"])
+        term = ' '.join([companyName, State, "contact", "company"])
         for url in search(term, num_results=1):
             return url
     except:
@@ -35,33 +35,38 @@ def contact_func(address):
 
     # creating driver
     owner = whois_script.whois_func(address)
-    owner = owner[:(owner.index("-"))]
+    ownerList = owner.split(" ")
+    x = 0
+    owner = ""
+    while not("-" in ownerList[x]):
+        owner += ownerList[x] + " "
+        x += 1
     
     # parsing together search query
     location = geolocation_script.geolocation_func(address)
-    location = location.replace(',', '').split(' ')
+    location = location.replace(' ', '').split(',')
     state = (set(location) & set(us_states)).pop()
 
     profile_url = getURL(owner, state)
     
 
-    # this will open the link
-    src =  requests.get(profile_url).content
+    # # this will open the link
+    # src =  requests.get(profile_url).content
 
     
 
-    # getting page source to parse
-    soup = BeautifulSoup(src, 'lxml')
+    # # getting page source to parse
+    # soup = BeautifulSoup(src, 'lxml')
 
-    # finding overview section
-    overview = soup.find('span', {'  dir': 'ltr','class': 'link-without-visited-state'}).getText(strip=True)
+    # # finding overview section
+    # overview = soup.find('span', {'  dir': 'ltr','class': 'link-without-visited-state'}).getText(strip=True)
 
-    web_loc = overview.findAll('a')
-    website = web_loc.get_text()
+    # web_loc = overview.findAll('a')
+    # website = web_loc.get_text()
 
-    src = requests.get(website).content
-    contact = soup.findAll(text='contact')
-    print(contact)
+    # src = requests.get(website).content
+    # contact = soup.findAll(text='contact')
+    # print(contact)
         
-contact_func('12.106.168.50')
+contact_func('12.111.29.162')
 
